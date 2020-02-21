cea26042156200afe0dd0973554b30dfa2ad3c8e
LeisaLey <6.yaxkin@gmail.com>
2020-02-21 08:45:45 -0600

    Must always have a commit message !!

--- autoloan/src/main/java/com/example/banking/AutoLoanApplication.java
+++ autoloan/src/main/java/com/example/banking/AutoLoanApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoLoanApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AutoLoanApplication.class, args);
     }
 
